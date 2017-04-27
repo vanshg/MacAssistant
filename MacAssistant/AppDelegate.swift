141e8a863cd2759fac8eb7efdb05e621dbca5868
Vansh Gandhi <vansh.gandhi@gmail.com>
2017-04-27 15:28:48 -0700

    Initial Commit

---
+++ MacAssistant/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacAssistant
+//
+//  Created by Vansh on 4/27/17.
+//  Copyright © 2017 vanshgandhi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacAssistant/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacAssistant
+//
+//  Created by Vansh on 4/27/17.
+//  Copyright © 2017 vanshgandhi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
