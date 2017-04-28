ee79c4938f8dd2f59572737980d17213a0ebd64a
Vansh Gandhi <vansh.gandhi@gmail.com>
2017-04-27 17:35:00 -0700

    Initial commit. Added OAuth, gRPC, SwiftProtobuf libraries

--- MacAssistant/AppDelegate.swift
+++ MacAssistant/AppDelegate.swift
@@ -7,18 +7,60 @@
 //
 
 import Cocoa
+import OAuthSwift
+import gRPC
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength)
+    let popover = NSPopover()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let icon = #imageLiteral(resourceName: "statusIcon")
+        icon.isTemplate = true
+        statusItem.image = icon
+        statusItem.action = #selector(statusIconClicked)
+        gRPC.initialize()
+        popover.contentViewController = ViewController(nibName: "ViewController", bundle: nil)
+        NSAppleEventManager.shared().setEventHandler(self, andSelector:#selector(AppDelegate.handleGetURL(event:withReplyEvent:)), forEventClass: AEEventClass(kInternetEventClass), andEventID: AEEventID(kAEGetURL))
+    }
+    
+    func handleGetURL(event: NSAppleEventDescriptor!, withReplyEvent: NSAppleEventDescriptor!) {
+        if let urlString = event.paramDescriptor(forKeyword: AEKeyword(keyDirectObject))?.stringValue, let url = URL(string: urlString) {
+            OAuthSwift.handle(url: url)
+        }
+    }
+    
+    func statusIconClicked(sender: AnyObject?) {
+        print("Status Icon Clicked")
+        togglePopover(sender: sender)
+    }
+    
+    func showPopover(sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+    }
+    
+    func closePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+    
+    func togglePopover(sender: AnyObject?) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover(sender: sender)
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        // We don't call shutdown() here because we can't be sure that
+        // any running server queues will have stopped by the time this is
+        // called. If one is still running after we call shutdown(), the
+        // program will crash.
+        // gRPC.shutdown()
     }
 
 
---
+++ MacAssistant/AssistantViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AssistantViewController.swift
+//  MacAssistant
+//
+//  Created by Vansh on 4/27/17.
+//  Copyright © 2017 vanshgandhi. All rights reserved.
+//
+
+import Cocoa
+
+class AssistantViewController: NSViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+}
