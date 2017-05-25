c49997daeb1ee37221dd25183f9ff4a5e1b538ab
Vansh Gandhi <vansh.gandhi@gmail.com>
2017-05-25 00:34:21 -0700

    Began implementation of settings screen

---
+++ MacAssistant/PreferencesWindowController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsViewController.swift
+//  MacAssistant
+//
+//  Created by Vansh on 5/22/17.
+//  Copyright © 2017 vanshgandhi. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class PreferencesWindowController: NSWindowController {
+    
+    @IBAction func didClickAdvancedTab(_ sender: Any) {
+        print("Advanced")
+    }
+    
+    @IBAction func didClickGeneralTab(_ sender: Any) {
+        print("General")
+    }
+    
+    
+}
