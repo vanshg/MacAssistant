7a7a52443dc24bcadc0161695eec760c77f2a8a7
Vansh Gandhi <vansh.gandhi@gmail.com>
2017-11-28 16:08:47 -0800

    Update proto script to work from current directory

--- gen_swift_proto.sh
+++ gen_swift_proto.sh
@@ -1,8 +1,10 @@
+# Need to cd into the root director where the proto files are stored, otherwise protoc complains
+cd googleapis
 protoc \
 	google/assistant/embedded/v1alpha1/embedded_assistant.proto \
 	google/rpc/status.proto \
-	--swiftgrpc_out=/Users/vanshgandhi/Desktop \
-	--swift_out=/Users/vanshgandhi/Desktop \
+	--swiftgrpc_out=../MacAssistant \
+	--swift_out=../MacAssistant \
 	--swift_opt=Visibility=Public \
-	--plugin=./grpc-swift/Plugin/protoc-gen-swiftgrpc \
-	--plugin=./grpc-swift/Plugin/protoc-gen-swift
+	--plugin=../grpc-swift/Plugin/protoc-gen-swiftgrpc \
+	--plugin=../grpc-swift/Plugin/protoc-gen-swift
