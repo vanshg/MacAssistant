772f1551f7eacc41312a647dda5d339c1ccbaf6f
Vansh Gandhi <vansh.gandhi@gmail.com>
2017-12-29 07:47:19 -0800

    Created generated folder. Made gen proto script a little more dynamic

--- gen_swift_proto.sh
+++ gen_swift_proto.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
 # Need to cd into the root director where the proto files are stored, otherwise protoc complains
+VERSION="v1alpha1"
+OUT_DIR="../MacAssistant/Generated/"
 cd googleapis
 protoc \
-	google/assistant/embedded/v1alpha1/embedded_assistant.proto \
+	google/assistant/embedded/$VERSION/embedded_assistant.proto \
 	google/rpc/status.proto \
-	--swiftgrpc_out=../MacAssistant \
-	--swift_out=../MacAssistant \
+	--swiftgrpc_out=$OUT_DIR \
+	--swift_out=$OUT_DIR \
 	--swift_opt=Visibility=Public \
 	--plugin=../grpc-swift/Plugin/protoc-gen-swiftgrpc \
 	--plugin=../grpc-swift/Plugin/protoc-gen-swift
